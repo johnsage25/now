f71bdcdc61b455551771a668bad5f0985bdb3e6e
John Nwanosike <pejonic@gmail.com>
2025-03-10 21:07:41 +0100

    Update run.bash

--- run.bash
+++ run.bash
@@ -301,10 +301,22 @@ function install_docker {
 }
 
 function install_docker_registry {
+
+    sudo apt update
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    sudo apt install docker-ce docker-ce-cli containerd.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo docker --version
+
     echo "Installing docker-registry..."
     sudo mkdir -p /var/lib/registry
     docker rm -f registry || true
-    docker run -d -p ${registryport}:${registryport} -e REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY=/var/lib/registry -v /var/lib/registry:/var/lib/registry --restart=always --name registry registry:2
+    docker run -d -p ${registryport}:${registryport} -e REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY=/var/lib/registry -v /var/lib/registry:/var/lib/registry --restart=always --name registry registry:latest
+    
 }
 
 function install_mongo {
